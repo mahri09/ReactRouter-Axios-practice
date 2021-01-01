d7afafab24455dcee5a6fcf0d60416b2b5f133f4
MAhri Atdayeva <mahriaa88@gmail.com>
2021-01-01 17:37:24 +0000

    get users from api with fetch and axios

--- src/UserList.js
+++ src/UserList.js
@@ -3,15 +3,47 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class UserList extends Component {
- 
-  render() {
-    return(
-      <div>
-        <h2>Users List</h2>
-    
+  state={
+    users: [],
+    isLoading: true
+  }
 
-      </div>
-    )
+  componentDidMount(){
+    // setTimeout(()=>{
+    //   fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(json => this.setState({
+    //     users: json, 
+    //     isLoading: false
+    // }))
+    // }, 1000);
+
+    setTimeout(()=>{
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((json) => {
+        this.setState({ 
+          users: json.data, 
+          isLoading: false
+        })
+      })
+    }, 1000);
+  }
+
+  render() {
+   return(
+     <div>
+      <h2>Users List</h2>
+      {
+        this.state.isLoading === true ? "Loading..."
+        :this.state.users.map((users)=>(
+          <div key={users.id}>
+            <p>{users.name} @{users.username}</p>
+          </div>
+        ))
+      }
+
+     </div>
+   )
   }
 
 }
